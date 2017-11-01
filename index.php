3a8d0589dc8dce92510e42d31eed3032d99bae6c
cforsythe <cforsythe@csumb.edu>
2017-11-01 23:38:25 +0000

    Showing all foods and prices

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include '../dbConnection.php';
+$conn = getDatabaseConnection();
+function getOptions(){
+    global $conn;
+    $sql = "SELECT timeName FROM `time`";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $record = $stmt->fetchAll();
+    foreach($record as $time){
+        echo "<option>" . $time['timeName'] . "</option>";
+    }
+}
+function printFoods(){
+    global $conn;
+    $sql = "SELECT foodName, price FROM `food`";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $record = $stmt->fetchAll();
+    echo "<table id='foods'>";
+    echo "<tr><th>Food</th>" . "<th>Price</th></tr>";
+    foreach($record as $food){
+        echo "<tr>
+                <td>" . $food['foodName'] . "</td>
+                <td>". $food['price'] . "</td>
+            </tr>";
+    }
+    echo "</table>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Food Menu </title>
+        <link rel='stylesheet' href='css/styles.css'>
+    </head>
+    <body>
+        <select>
+            <option value="">Select One</option>
+            <?=getOptions()?>
+        </select>
+        <?=printFoods()?>
+    </body>
+</html>
